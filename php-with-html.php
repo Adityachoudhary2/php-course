34900bc458c20ddd0cb0115d7a7e8ac59868465c
Aditya <choudharyaditya9381@gmail.com>
2025-04-07 21:51:33 +0530

    php include in html tag

---
+++ php-with-html.php
@@ -0,0 +1,28 @@
+
+ <!-- Add Html tag in php  -->
+<?php
+
+echo "<h1>Hello, World!</h1>";
+echo "<p>This is a simple PHP script that outputs HTML.</p>";
+?>
+
+
+<?php
+ $name = "Aditya choudhary  ";
+ 
+ echo "<h2 style='color:orange'> Welcome, $name!</h2>";
+
+?>
+
+<!-- Add php tag in html -->
+ <?php
+        $h2_color = "gray";
+        ?>
+ <h1 style="color:<?php echo $h2_color; ?>">
+
+        <?php
+       $name = "Aditya choudhary  "; 
+        echo "Hello, World! $name!";
+    ?>  
+ </h1>
+
