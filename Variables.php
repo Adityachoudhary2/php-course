ba555729c0bd384a80629ab64910651e50c31d1a
Aditya <choudharyaditya9381@gmail.com>
2025-04-07 21:23:46 +0530

    Variables 2 type example

---
+++ Variables.php
@@ -0,0 +1,37 @@
+<!-- <?php
+// $name = "Aditya"; // string
+
+
+// echo $name; // Aditya
+// echo "<br>";
+// echo $name; // Aditya
+// echo "<br>";
+// echo $name; // Aditya
+// echo "<br>";
+// echo $name; // Aditya
+// echo "<br>";
+
+?> -->
+
+
+<!-- second example -------------------- -->
+<?php
+$num = 10; // string
+
+echo $num * 2; // Aditya
+echo "<br>";
+echo $num * 3; // Aditya
+echo "<br>";
+echo $num * 4; // Aditya
+echo "<br>";
+echo $num * 5; // Aditya
+echo "<br>";
+echo $num * 6; // Aditya
+echo "<br>";
+echo $num * 7; // Aditya
+echo "<br>";
+
+
+?>
+
+
