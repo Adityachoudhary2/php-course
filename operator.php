b5dec669a665263af31e1375ba40a4523e6f4cb3
Aditya <choudharyaditya9381@gmail.com>
2025-04-08 20:55:06 +0530

    operator

---
+++ operator.php
@@ -0,0 +1,10 @@
+  <!-- 
+  Arithmetic operators
+  Assignment operators
+  Comparison operators
+  Increment/Decrement operators
+  Logical operators
+  String operators
+  Array operators conditional
+  Assignment operators 
+  -->
