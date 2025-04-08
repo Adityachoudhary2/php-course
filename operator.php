101ea6d6b085432ec21d91886e1041071e3d4398
Aditya <choudharyaditya9381@gmail.com>
2025-04-08 21:22:40 +0530

    define operator

--- operator.php
+++ operator.php
@@ -1,10 +1,20 @@
   <!-- 
   Arithmetic operators
+     +, - , * , / , %
+
   Assignment operators
+     =, +=, -=, *=, /=, %=
   Comparison operators
+        ==, ===, !=, !==, >, <, >=, <=
   Increment/Decrement operators
+        ++, --
   Logical operators
+        &&, ||, !
   String operators
+        . , .=  
   Array operators conditional
+    operators
+            +, ==, ===, !=, !==, <=>, >=, <=>
   Assignment operators 
+        =, +=, -=, *=, /=, %=
   -->
