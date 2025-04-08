602949016577fe6c22d6e47b3b759eab2fd0b7d7
Aditya <choudharyaditya9381@gmail.com>
2025-04-08 12:56:08 +0530

    const

---
+++ constant.php
@@ -0,0 +1,16 @@
+<?php
+const APP_NAME = 'My Application';
+const APP_VERSION = '1.0.0';
+const APP_AUTHOR = 'John Doe';
+
+echo "Welcome to ",APP_NAME . "!<br>";
+echo "Version: " . APP_VERSION . "<br>";   
+echo "Author: " . APP_AUTHOR . "<br>";
+echo "This application is designed to help you manage your tasks efficiently.\n";   
+?>
+
+<?php
+
+
+
+?>
