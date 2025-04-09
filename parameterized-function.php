2d18898e052901e961794e2b3d38485af5592a9c
Aditya <choudharyaditya9381@gmail.com>
2025-04-09 23:05:29 +0530

    parameterizes-function

---
+++ parameterized-function.php
@@ -0,0 +1,21 @@
+<?php
+
+
+//  first method ------------------
+// function sum (){
+// echo  10 +20;
+// }
+// sum();
+
+//  second method ------------------
+
+// function sum ($a,$b){
+//     echo  $a + $b;
+//     }
+//     sum(2,2);
+
+
+//  third method ------------------
+
+
+?>
