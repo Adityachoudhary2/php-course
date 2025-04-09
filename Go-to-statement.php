290d8b0ed2a8c8883b24dd677033e0401617ef65
Aditya <choudharyaditya9381@gmail.com>
2025-04-09 21:57:02 +0530

    go to statement

---
+++ Go-to-statement.php
@@ -0,0 +1,14 @@
+ <?php
+ $a =10;
+ if($a+2 ==12){
+    goto aditya;
+    echo "This is a goto after condition statement<br>";
+    return;
+
+ }
+//  goto statement is used to jump to a specific part of the code
+
+ aditya:
+ echo "This is a goto statement<br>";
+
+ ?>
