2695e458955eb7e38fe382d5a15cc34650f6c902
Aditya <choudharyaditya9381@gmail.com>
2025-04-13 20:59:37 +0530

    signup form

---
+++ PHP_form/signup.php
@@ -0,0 +1,11 @@
+<?php
+if($_POST){
+    echo "user name is  ".$_POST['username'] . "<br>";
+    echo "user email is  ".$_POST['email'] . "<br>";
+    echo "user password is  ".$_POST['password'] . "<br>";
+  
+
+
+
+}
+?>
