80e3e2e22ae764c151be7a11e933c830f26e0e9f
Aditya <choudharyaditya9381@gmail.com>
2025-04-14 10:55:37 +0530

    super global variable

---
+++ superGlobal.php
@@ -0,0 +1,18 @@
+
+
+<?php
+ $a = 10;
+ echo "<pre>";
+//  print_r($GLOBALS);
+// print_r($_SERVER);
+// print_r($_REQUEST);
+// print_r($_POST);
+// print_r($_GET);
+// print_r($_FILES);
+// print_r($_COOKIE);
+// print_r($_SESSION);
+// print_r($_ENV);
+
+
+ echo "<pre>";
+ ?> 
