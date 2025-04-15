ee17fcf32caca9880f39b055de9019f96f57ce01
Aditya <choudharyaditya9381@gmail.com>
2025-04-15 13:44:53 +0530

    make a website

---
+++ website/footer.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+    
+
+<p>&copy; 2023 My Website</p>
+    <p>Follow us on social media:</p>
+</footer>
---
+++ website/index.php
@@ -0,0 +1,25 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="style.css">
+ </head>
+ <body>
+   <?php
+    include_once('header.php');
+    ?>
+
+    <div class="container" >
+     <h1 class="heading">Home page </h1>
+     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam illum architecto itaque dolore vel aliquam, doloremque quam adipisci atque sapiente, cumque magni error, reiciendis fugit nihil perspiciatis saepe assumenda neque voluptas quibusdam quae? Dolore voluptatum fugit et quibusdam consequatur, quod minus corporis magnam expedita nostrum odio aliquam nesciunt earum numquam quaerat ab maxime assumenda? Consequatur magni ex molestias impedit exercitationem accusamus beatae minima! Expedita eveniet, odio tempora tempore quaerat voluptate nesciunt, exercitationem ullam suscipit, mollitia vero placeat? Magni architecto culpa impedit? Quisquam, ipsa harum enim cum, saepe quasi recusandae necessitatibus ex non laborum cupiditate dolorum illum. Eum quasi ullam doloribus facilis! Facere odio ipsam quo? Ducimus, sunt! Aliquid facilis quisquam quaerat beatae voluptate repudiandae, corrupti exercitationem ipsum in aperiam debitis maxime error praesentium minus aliquam officiis accusantium laudantium hic vel. Laborum reprehenderit nemo dignissimos quod omnis ea sit, officiis rerum fuga eveniet similique adipisci. Assumenda deserunt ea tempore laboriosam rem nobis temporibus, quam fugit labore corrupti! Ad cum totam odit quae ab, eius dicta facere excepturi? Odit nemo harum cum neque? Provident eos non itaque! Unde aliquam ipsam cupiditate ut perspiciatis et aspernatur ratione! Nostrum quaerat corporis ut ducimus enim officia, fugiat ipsam optio rerum ea quam esse corrupti recusandae.</p>
+     <h3>Where does it come form ?</h3>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae iusto aspernatur commodi id sint minima earum nisi. Delectus sint hic autem quam officiis provident necessitatibus nisi ex dolores ipsam est, vitae quod consequuntur a itaque et totam corrupti! Reiciendis voluptates quia temporibus soluta rerum tenetur nulla exercitationem asperiores. Pariatur labore quam animi modi id doloremque deleniti cupiditate consectetur cum officiis beatae voluptate quo facere expedita, nobis ipsam? Distinctio quas possimus quaerat, excepturi consequatur perferendis tempore reiciendis enim optio necessitatibus animi molestias neque laborum maxime ex impedit? Ratione eum amet ullam deserunt obcaecati quae facere ipsa possimus assumenda, minima est labore.</p>
+     </div>
+    <?php
+    include_once('footer.php');
+    ?>
+   
+ </body>
+ </html>
