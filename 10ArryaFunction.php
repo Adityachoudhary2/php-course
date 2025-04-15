7f05e6e333710bc899f5664c9c58167823f4f0ca
Aditya <choudharyaditya9381@gmail.com>
2025-04-15 09:46:22 +0530

    10 array function

---
+++ 10ArryaFunction.php
@@ -0,0 +1,13 @@
+ <?php
+  $user = ['aditya', 'sanjay', 'sachin', 'prashant', 'abhishek'];
+  
+
+//   if(is_array($user)){
+//     echo "This is an array<br>";
+//   }
+
+remove element in all array any element
+unset($user[0]);
+// print_r($user);
+
+ ?>
