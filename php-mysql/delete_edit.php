f9b0e1ecb57d3d4aba2ac18774d315667c263072
Aditya <choudharyaditya9381@gmail.com>
2025-04-19 23:11:05 +0530

    sql data detele by php

---
+++ php-mysql/delete_edit.php
@@ -0,0 +1,47 @@
+<?php
+ include("./config.php");
+
+ $students=$conn->prepare("SELECT * FROM students");
+ $students->execute();
+ $result=$students->fetchAll();
+
+ echo "<table border='4'>";
+ echo "<tr>
+ <th>ID</th>
+ <th>Name</th>
+ <th>Email</th>
+ <th>City</th>
+ <th>Course</th>
+ <th>Delete</th>
+ </tr>";
+ foreach($result as $student){
+    echo "<tr>
+    <td>".$student['id']."</td>
+    <td>".$student['name']."</td>
+    <td>".$student['email']."</td>
+    <td>".$student['city']."</td>
+    <td>".$student['course']."</td>
+
+    <td>
+    <form  method='POST'>
+    <button name='delete' value=".$student['id']."'>Delete</button>
+    </form>
+    </td>;
+
+    
+</tr>";
+
+}
+echo "</table>";
+if(isset($_POST['delete'])){
+$id=$_POST['delete'];
+$students=$conn->prepare("delete from students where id=:id");
+}
+if($students->execute([':id'=>$id])){
+    echo "Deleted successfully";
+
+
+
+  }
+
+ ?>
