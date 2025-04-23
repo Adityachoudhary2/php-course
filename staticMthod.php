b82f251a22f99e5df1b52feb547f8cae820d19e2
Aditya <choudharyaditya9381@gmail.com>
2025-04-24 00:10:31 +0530

    static method

---
+++ staticMthod.php
@@ -0,0 +1,26 @@
+ <?php
+ //simple method
+//  class Honda{
+//     function companyName(){
+//         echo "Honda";
+//     }
+//     //function 2
+//  }
+
+//  $Honda= new Honda();
+//     $Honda->companyName();
+
+
+
+//static method
+class Honda {
+    static function companyName() {
+        echo "Honda";
+    }
+}
+
+
+ Honda::companyName(); //calling static method
+
+
+ ?>
